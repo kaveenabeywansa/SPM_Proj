47e26c1461df40f7abb7bf7c2cfe0a48235c0d4d
minila234 <gamageminila@gmail.com>
2018-08-28 14:18:42 +0530

    create profile UI

---
+++ profile.php
@@ -0,0 +1,106 @@
+<!Doctype html>
+<html>
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+	<title>Home</title>
+	<!-- Meta tag Keywords -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8" />
+	<meta name="keywords" content="Fabricate Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design"
+	/>
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- //Meta tag Keywords -->
+
+	<!-- Custom-Files -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Bootstrap-Core-CSS -->
+	<link rel="stylesheet" href="css/lightbox.css">
+	<!-- gallery light box -->
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	<link rel="stylesheet" href="css/style1.css" type="text/css" media="all" />
+	<!-- Style-CSS -->
+	<link rel="stylesheet" href="css/fontawesome-all.css">
+	<!-- Font-Awesome-Icons-CSS -->
+	<!-- //Custom-Files -->
+
+	<!-- Web-Fonts -->
+	<link href="//fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+	<!-- //Web-Fonts -->
+
+</head>
+
+<body data-spy="scroll" data-target=".fixed-top">
+	<!-- header -->
+	<header>
+		<!-- navbar -->
+		<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="index.html">
+				<span>I</span>ntern<span>s</span>hip</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+			 aria-expanded="false" aria-label="Toggle navigation">
+				<i class="fas fa-bars"></i>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto text-center mr-lg-5">
+					<li class="nav-item active mr-lg-2 mb-lg-0 mb-2">
+						<a class="nav-link" href="index.html">Home
+							<span class="sr-only">(current)</span>
+						</a>
+					</li>
+					<li class="nav-item mr-lg-2 mb-lg-0 mb-2">
+						<a class="nav-link scroll" href="#about">About Us</a>
+					</li>
+					<li class="nav-item mr-lg-2 mb-lg-0 mb-2">
+						<a class="nav-link scroll" href="#services">Services</a>
+					</li>
+					<li class="nav-item mr-lg-2 mb-lg-0 mb-2">
+						<a class="nav-link scroll" href="#team">Our Team</a>
+					</li>
+					<li class="nav-item mr-lg-2 mb-lg-0 mb-2">
+						<a class="nav-link scroll" href="#contact">Contact Us</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="login.html">Sign In | Sign Up</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="profile.php">Profile</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<!-- //navbar ends here -->
+	</header>
+	<!-- //header -->
+
+<body class="banner-w3l">
+
+<div class="title-agile">
+
+        <h1 class="title-agile text-center">Student Profile</h1>
+    
+	</div>
+<div class="login-wrap">
+<div class="main_w3agile">
+
+<?php
+		echo "<h1>"."Student profile"."</h1>";
+?>
+</div>
+</div>
+</body>
+
+
+</html>
