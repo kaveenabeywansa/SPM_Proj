070619dde8b94d223978a3f9a5faba9c10bd8217
Kaveen Abeywansa <kaveenabeywansa@gmail.com>
2018-08-30 00:36:25 +0530

    covert to php

---
+++ supdowni3form.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Form Submission</title>
+	<!-- Meta tag Keywords -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8" />
+	<meta name="keywords" content="Fabricate Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design"
+    />
+    
+    <!-- Custom-Files -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Bootstrap-Core-CSS -->
+	<link rel="stylesheet" href="css/lightbox.css">
+	<!-- gallery light box -->
+	<link rel="stylesheet" href="css/style1.css" type="text/css" media="all" />
+	<!-- Style-CSS -->
+    <link rel="stylesheet" href="css/fontawesome-all.css">
+    <link rel="stylesheet" type="text/css" href="css/supervisor.css">
+	<!-- Font-Awesome-Icons-CSS -->
+    
+    <!-- Web-Fonts -->
+	<link href="//fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    <!-- //Web-Fonts -->
+    
+</head>
+
+<body class="container-fluid">
+	<!-- header -->
+		<!-- navbar -->
+		<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="index.html">
+				<span>I</span>ntern<span>s</span>hip</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+			 aria-expanded="false" aria-label="Toggle navigation">
+				<i class="fas fa-bars"></i>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto text-center mr-lg-5">
+					<li class="nav-item mr-lg-2 mb-lg-0 mb-2">
+						<a class="nav-link" href="">Link 1</a>
+					</li>
+					<li class="nav-item mr-lg-2 mb-lg-0 mb-2">
+						<a class="nav-link" href="">Link 2</a>
+					</li>
+					<li class="nav-item mr-lg-2 mb-lg-0 mb-2 active">
+						<a class="nav-link" href="">Attest I-3 Forms
+                            <span class="sr-only">(current)</span>
+                        </a>
+					</li>
+					<!-- <li class="nav-item mr-lg-2 mb-lg-0 mb-2">
+						<a class="nav-link" href="formI-3.php">I-3 form</a>
+					</li>
+					<li class="nav-item mr-lg-2 mb-lg-0 mb-2">
+						<a class="nav-link" href="formI-6.php">I-6 form</a>
+					</li> -->
+					<li class="nav-item">
+						<a class="nav-link" href="login.html">Sign In | Sign Up</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<div class="supform">
+        <center>
+            <h1>Download & Re-Upload I-3 Forms</h1>
+        </center>
+        <table>
+            <tr>
+                <th width="20%">Reg. No.</th>
+                <th width="50%">Name</th>
+                <th width="15%">Download</th>
+                <th width="15%">Upload</th>
+            </tr>
+            <tr>
+                <td>01</td>
+                <td>Bruce Wayne</td>
+                <td>
+                    <center>
+                        <img src="images/downloadicon.png" alt="Download Form" height="50" width="50">
+                    </center>
+                </td>
+                <td>
+                    <center>
+                        <img src="images/uploadicon.png" alt="Upload Edited Form" height="50" width="50">
+                    </center>
+                </td>
+            </tr>
+            <tr>
+                <td>02</td>
+                <td>Kent Clark</td>
+                <td>
+                    <center>
+                        <img src="images/downloadicon.png" alt="Download Form" height="50" width="50">
+                    </center>
+                </td>
+                <td>
+                    <center>
+                        <img src="images/uploadicon.png" alt="Upload Edited Form" height="50" width="50">
+                    </center>
+                </td>
+            </tr>
+            <tr>
+                <td>03</td>
+                <td>Tony Stark</td>
+                <td>
+                    <center>
+                        <img src="images/downloadicon.png" alt="Download Form" height="50" width="50">
+                    </center>
+                </td>
+                <td>
+                    <center>
+                        <img src="images/uploadicon.png" alt="Upload Edited Form" height="50" width="50">
+                    </center>
+                </td>
+            </tr>
+            <tr>
+                <td>04</td>
+                <td>Peter Parker</td>
+                <td>
+                    <center>
+                        <img src="images/downloadicon.png" alt="Download Form" height="50" width="50">
+                    </center>
+                </td>
+                <td>
+                    <center>
+                        <img src="images/uploadicon.png" alt="Upload Edited Form" height="50" width="50">
+                    </center>
+                </td>
+            </tr>
+        </table>
+        </div>
+
+</body>
+</html>
